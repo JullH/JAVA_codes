f227be6a2fe5aa2815def9b28daf077db640687a
JullH <h17586819273@gmail.com>
2024-04-18 17:32:00 +0800

    .

--- src/day27/sample/ConnectionSample.java
+++ src/day27/sample/ConnectionSample.java
@@ -12,8 +12,11 @@ import java.sql.SQLException;
 public class ConnectionSample {
     public static void main(String[] args) {
         try {
+            //1.加载并注册JDBC驱动
             Class.forName("com.mysql.cj.jdbc.Driver");
+            //2.创建数据库连接
             String url = "jdbc:mysql://localhost:3306/imooc?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serviceTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true";
+            
             Connection conn = DriverManager.getConnection(url,"root","123456");
             System.out.println(conn);
         } catch (ClassNotFoundException e) {
