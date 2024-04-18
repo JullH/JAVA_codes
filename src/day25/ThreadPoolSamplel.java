5a7bd74a6de9118c377e763d4dd3c5e104351cc5
JullH <h17586819273@gmail.com>
2024-04-18 16:01:20 +0800

    线程

---
+++ src/day25/ThreadPoolSamplel.java
@@ -0,0 +1,25 @@
+package src.day25;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @version 1.0
+ * @auther JullH
+ */
+
+public class ThreadPoolSamplel {
+    public static void main(String[] args) {
+       ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 1000; i++) {
+            int finalI = i;
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + finalI);
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+}
---
+++ src/day26/Homework02.java
@@ -0,0 +1,10 @@
+package src.day26;
+
+/**
+ * @version 1.0
+ * @auther JullH
+ */
+
+public class Homework02 {
+
+}
