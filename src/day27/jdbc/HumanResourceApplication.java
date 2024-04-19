013516866884c07e6c3b54da998189a1319c2a69
JullH <h17586819273@gmail.com>
2024-04-19 09:20:21 +0800

    查询部门名称

--- src/day27/jdbc/HumanResourceApplication.java
+++ src/day27/jdbc/HumanResourceApplication.java
@@ -1,5 +1,10 @@
 package src.day27.jdbc;
 
+import src.day27.jdbc.command.Command;
+import src.day27.jdbc.command.QueryCommand;
+
+import java.util.Scanner;
+
 /**
  * @version 1.0
  * @auther JullH
@@ -7,6 +12,14 @@ package src.day27.jdbc;
 
 public class HumanResourceApplication {
     public static void main(String[] args) {
-
+        System.out.println("1-查询部门名称：");
+        Scanner scanner = new Scanner(System.in);
+        Integer cmd = scanner.nextInt();
+        switch (cmd){
+            case 1:
+                Command Command = new QueryCommand();
+                Command.execute();
+                break;
+        }
     }
 }
---
+++ src/day27/jdbc/command/QueryCommand.java
@@ -0,0 +1,69 @@
+package src.day27.jdbc.command;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * @version 1.0
+ * @auther JullH
+ */
+
+public class QueryCommand implements Command{
+    @Override
+    public void execute() {
+        System.out.println("输入部门名称：");
+        Scanner scanner = new Scanner(System.in);
+        String pdname = scanner.next();
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            //1.加载并注册JDBC驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //2.创建数据库连接
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/imooc?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serviceTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true","root","123456");
+            //3.创建statement对象
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("select * from employee where dname = '" + pdname + "'");
+
+            //4.遍历查询结果
+            while (rs.next()){
+                Integer eno = rs.getInt(1);
+                String ename = rs.getString("ename");
+                float salary = rs.getFloat("salary");
+                String dname = rs.getString("dname");
+                System.out.println(dname + "-" + eno + "-" + ename + "-" + salary);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(rs != null){
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if(stmt != null){
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
